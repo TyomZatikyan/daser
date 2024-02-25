13e9b5488030d410dd7f476743acc79df8c732c9
TyomZatikyan <159648052+TyomZatikyan@users.noreply.github.com>
2024-02-25 20:42:23 +0400

    173,174,184

---
+++ daser_python_9.py
@@ -0,0 +1,39 @@
+# # # # # Ex 173 # # # # #
+
+################################################
+
+# def getUserInput():
+#     return int(input("Please enter number: "))
+
+
+# def numCounter(n):
+#     if n == -1: 
+#         return 0
+#     else:
+#         return (n + numCounter(getUserInput()))
+
+# print(numCounter(getUserInput()))
+
+################################################
+
+# # # # # Ex 174 # # # # #
+
+################################################
+
+# # # # # Ex 184 # # # # #
+
+# def flatten(data):
+#     m_list = []
+#     for i in data:
+#         if isinstance(i, list):
+#             m_list.extend(flatten(i))
+#         else:
+#             m_list.append(i)
+
+#     return m_list
+
+# data = [1, [2, 3], [4, 5, 6, [7, 8, [9, 10]]]]
+# print(flatten(data))
+
+################################################
+
