0a8a425c9cae838f22135e89fbcd74c0f7cde7d5
TyomZatikyan <159648052+TyomZatikyan@users.noreply.github.com>
2024-03-31 23:48:11 +0400

    daser

---
+++ daser_python15.py
@@ -0,0 +1,69 @@
+# class CovidTest:
+#     def init(self, celsius):
+#         self.celsius = celsius
+    
+#     def check(self):
+#         if a >= 120 and a <= 128:
+#             print('corona virus')
+#         else:
+#             print('corona ches')
+
+    
+# a = int(input('jermutyun: '))
+# ctest = CovidTest()
+# ctest.check()
+
+
+# import math
+# class Dict:
+
+#     letter_values = {
+#             'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
+#             'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 1,
+#             'k': 2, 'l': 3, 'm': 4, 'n': 5, 'o': 6,
+#             'p': 7, 'q': 8, 'r': 9, 's': 1, 't': 2,
+#             'u': 3, 'v': 4, 'w': 5, 'x': 6, 'y': 7,
+#             'z': 8
+#         }
+
+#     def count_items(self, name):
+#         count = 0
+#         nameCount = 0
+#         while (count < len(name)):
+#             nameCount += self.letter_values[name[count]]
+#             count += 1
+#         if math.sqrt(nameCount) < 5:
+#             print("yes")
+#         else:
+#             print("no")
+
+
+# name = input('enter your text: ')
+# dict_instance = Dict()
+# dict_instance.count_items(name)
+
+
+# class Rectangle:
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#     def makares(self):
+#         return self.length * self.width
+
+# number1 = int(input("enter number:"))
+# number = int(input('enter number : '))
+# rectangle_instance = Rectangle(number,number1)
+# print(rectangle_instance.makares())
+
+# class Circle:
+#     def __init__(self):
+#         pass
+
+#     def calc(self, radius):
+#         return radius*2
+
+# r = int(input("input: "))
+# circle = Circle()
+# print(circle.calc(r))
+    
+    
